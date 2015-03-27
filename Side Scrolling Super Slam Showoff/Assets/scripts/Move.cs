a535872d5dc5cca31a65e13bc6b09aa1e61c0505
NLferdiNL <nlferdinl@gmail.com>
2015-03-27 19:13:57 +0100

    Rev 0.3
    Big Update, menu in progress. Max teleports added.
    Several small updates added. Fixed wall-jumping.

---
+++ Side Scrolling Super Slam Showoff/Assets/scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public float Max;
+	public Transform me;
+	public int speed;
+	private Rigidbody2D rb2d;
+
+	void Awake(){
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.fixedAngle = true;
+	}
+
+	void FixedUpdate() {
+		float x = Input.GetAxis("Horizontal");
+		Vector2 pos = new Vector2 (x, 0f) * speed;
+		rb2d.AddForce (pos);
+		me.position = new Vector3(Mathf.PingPong(Time.time, Max), transform.position.y, transform.position.z);
+	}
+}
--- Side Scrolling Super Slam Showoff/Assets/scripts/PlayerMovement.cs
+++ Side Scrolling Super Slam Showoff/Assets/scripts/PlayerMovement.cs
@@ -1,42 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {	
 	public float speed;
-	public float jumpheight;
+	public float jumpHeight;
+	public int maxTeleports;
+	public Text teletext;
 	private Transform player; 
 	private bool jumping = false;
-
-	Rigidbody2D rb2d;
+	private Rigidbody2D rb2d;
 
 	void Awake()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
 		player = this.transform;
-		rb2d.fixedAngle = true;
+		teletext.text = "Teleports left: " + maxTeleports;
 	}
 	
 	void FixedUpdate()
 	{
 		float x = Input.GetAxis("Horizontal");
 		Vector2 pos = new Vector2 (x, 0f) * speed;
-		//Debug.Log (pos);
-		if (jumping == false) {
-			rb2d.AddForce (pos);
-		}
+		rb2d.AddForce (pos);
 	}
+
 
 
 	//Jump checks
 	void OnCollisionExit2D(Collision2D other){
 		jumping = true;
-		Debug.Log ("Touching is false");
+		//Debug.Log ("Touching is false");
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
-		if (other.gameObject.tag == "Terrain" && other.gameObject.transform.position.y < player.position.y){
+		if (other.gameObject.tag == "Terrain"){
 			jumping = false;
-			Debug.Log ("Touching is true");
+			//Debug.Log ("Touching is true");
 		}
 	}
 	
@@ -44,27 +44,53 @@ public class PlayerMovement : MonoBehaviour {
 		if (other.gameObject.tag == "Terrain" && other.gameObject.transform.position.y < player.position.y && other.gameObject.transform.position.y + player.localScale.y == player.position.y)
 			 {
 			jumping = false;
-			Debug.Log ("Touching is true");
+			//Debug.Log ("Touching is true");
 		}
 	}
 
+	IEnumerator alert(float timeToWait = 0.1f){
+		teletext.color =  new Color(255,0,0);
+		yield return new WaitForSeconds(timeToWait);
+		teletext.color = new Color(255,255,255);
+		yield return new WaitForSeconds(timeToWait);
+		teletext.color =  new Color(255,0,0);
+		yield return new WaitForSeconds(timeToWait);
+		teletext.color = new Color(255,255,255);
+		yield return new WaitForSeconds(timeToWait);
+		teletext.color =  new Color(255,0,0);
+		yield return new WaitForSeconds(timeToWait);
+		teletext.color = new Color(255,255,255);
+		yield return new WaitForSeconds(timeToWait);
+	}
+	
 	void Update()
 	{
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		Debug.DrawLine(player.position, new Vector3(mousePos.x, mousePos.y, player.position.z));
+		RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 		if (Input.GetMouseButtonDown (0)) {
-			RaycastHit2D rayCast = Physics2D.Raycast(mousePos, Vector2.zero);
-			if(rayCast.collider == null) {
-				player.position = new Vector3(mousePos.x,mousePos.y,player.position.z);
+			if(maxTeleports > 0){
+				if (hit.collider != null){
+					print("Hit: " + hit.collider.gameObject.name);
+				}else{
+					player.position = new Vector3(mousePos.x,mousePos.y,player.position.z);
+					maxTeleports--;
+					if(maxTeleports == 0){
+						teletext.text = "You've got no teleports left!";
+					} else {
+						teletext.text = "Teleports left: " + maxTeleports;
+					}
+				}
 			} else {
-				Debug.Log ("Struck something at: " + rayCast.collider.gameObject.transform.position);
+				Debug.Log ("hi");
+				StartCoroutine(alert());
 			}
-			//Debug.Log("Position " + mousePos);
+		}
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Application.LoadLevel(Application.loadedLevel);
 		}
 		if (Input.GetKeyDown ("space") && jumping == false) {
-			Vector2 jump = new Vector2(0f,jumpheight);
+			Vector2 jump = new Vector2(0f,jumpHeight);
 			rb2d.AddForce(jump, ForceMode2D.Impulse);
 		}
-		//Debug.Log ("Jumping = " + jumping);
 	}
 }
